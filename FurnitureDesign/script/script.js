3904c529fc2fbb928381e78cdfe7a23d66990732
toge-ma-ru <92039067+toge-ma-ru@users.noreply.github.com>
2021-10-22 17:06:39 +0900

    Add files via upload

---
+++ FurnitureDesign/script/script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$('#nav-icon').click(function() {
+		$('#menu-wrapper').toggleClass('opened');
+	});
+
+	$('#mask').click(function() {
+		$('#menu-wrapper').toggleClass('opened');
+	});
+});
